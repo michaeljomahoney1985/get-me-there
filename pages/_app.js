d4697088fc33e026bef84f9d5dc18979b3d0116e
michaeljomahoney1985 <michaeljomahoney1985@gmail.com>
2025-12-17 23:14:21 +0000

    _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,26 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function () {
+                  var script = document.createElement("script");
+                  script.async = 1;
+                  script.src = 'https://emrldtp.com/NDgyMjY4.js?t=482268';
+                  document.head.appendChild(script);
+              })();
+            `,
+          }}
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
+
+export default MyApp
